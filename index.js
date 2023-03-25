049ad0cffff79b2078df6c8f190c0d78aa840392
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-03-25 13:44:44 +0000

    responsive navigation is created

--- index.js
+++ index.js
@@ -64,3 +64,16 @@ function showMore() {
 }
 
 showMore()
+
+
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".home-page_navigation")
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
--- post.js
+++ post.js
@@ -74,7 +74,17 @@ function showMore() {
 
 showMore()
 
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".home-page_navigation")
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
 
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
 
 
 

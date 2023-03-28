2a38b0c9e859c2d1d054a2b83696acc05aaf5d18
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-03-28 14:13:19 +0100

    separation of concerns id done

--- index.js
+++ index.js
@@ -7,16 +7,4 @@ import {showMore} from "./utils.js"
 saveImgToLs()
 setInitialStyle()
 showMore()
-
-
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".home-page_navigation")
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+setHamburgerMenu()
